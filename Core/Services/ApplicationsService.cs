0ae622c723b58523390cbd4bed27eee237943ebc
unknown <yrezehi@gmail.com>
2023-10-15 07:04:15 -0700

    setup: repos

--- Core/Services/ApplicationsService.cs
+++ Core/Services/ApplicationsService.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Models;
 
 namespace Core.Services
 {
     public class ApplicationsService
     {
-        public void Register()
+        public void Register(Application application)
         {
 
         }
