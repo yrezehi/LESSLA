be01b3b2a4187c02d3fa95fb525ddb94127c958e
unknown <yrezehi@gmail.com>
2023-10-15 04:23:32 -0700

    setup: core

---
+++ UI/Controllers/LogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    [Route("[controller]")]
+    public class LogsController : Controller
+    {
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Index() =>
+            Ok();
+    }
+}
