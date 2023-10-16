e8ff36eece010a57633c658babf5407922bf4fdd
unknown <yrezehi@gmail.com>
2023-10-16 06:57:35 -0700

    add: ControllerRegistry

---
+++ Server/Controllers/ControllerRegistry.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Controllers
+{
+    public static class ControllerRegistry
+    {
+        public static void RegisterControllers(this WebApplication application) =>
+            application.Index();
+
+        private static void Index(this WebApplication application) =>
+            application.MapGet("/", () => "");
+    }
+}
