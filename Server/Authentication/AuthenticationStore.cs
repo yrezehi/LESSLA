248efc74e4396ce1d2bc2229d77242e8f8b00b79
unknown <yrezehi@gmail.com>
2023-10-17 07:27:36 -0700

    add: AuthenticationStore

---
+++ Server/Authentication/AuthenticationStore.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Server.Authentication
+{
+    public class AuthenticationStore
+    {
+        private readonly IMemoryCache Cache;
+
+        public AuthenticationStore(IMemoryCache cache) =>        
+            Cache = cache;
+        
+        public void SetValue(string key, T value, int ExpirationInSeconds) =>
+            Cache.Set(key, value, TimeSpan.FromSeconds(ExpirationInSeconds));
+        public bool ContainsKey(string key) =>
+            Cache.TryGetValue(key, out var value);
+
+        public T? GetValue<T>(string key) =>
+            Cache.TryGetValue(key, out T value) ? value : default;
+
+        public void RemoveValue(string key) => 
+            Cache.Remove(key);
+    }
+}
