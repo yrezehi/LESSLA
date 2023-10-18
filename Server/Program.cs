0da807ae55cd8f08a8fa615859b54fe47a1ef188
unknown <yrezehi@gmail.com>
2023-10-18 08:22:29 -0700

    add: digo.seri

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,10 @@
 using Sample.Extensions;
 using Server.Controllers;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Serilog.Debugging.SelfLog.Enable(TextWriter.Synchronized(File.CreateText("C:\\Users\\Administrator\\Documents\\Projects\\LESSLA\\logs.txt")));
 
 builder.RegisterConfiguration();
 
