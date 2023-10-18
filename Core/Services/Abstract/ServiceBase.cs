b1fe9835ed9e2dd461cad22b0009c314da69cb92
unknown <yrezehi@gmail.com>
2023-10-18 09:54:58 -0700

    setup: base services

---
+++ Core/Services/Abstract/ServiceBase.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection;
+using Core.Services.Abstract.Interfaces;
+
+namespace Core.Services.Abstract
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class
+    {
+        public ServiceBase(IRDBMSUnitOfWork unitOfWork) =>
+            (DBSet, UnitOfWork) = (unitOfWork.Repository<T>().DBSet, unitOfWork);
+
+        protected internal IRDBMSUnitOfWork UnitOfWork { get; set; }
+        protected DbSet<T> DBSet { get; set; }
+
+        private static int DEFAULT_PAGE_SIZE = 10;
+
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression) =>
+            DBSet.Where(expression).IncludeSurface();
+
+        public virtual bool Any(Expression<Func<T, bool>> expression) =>
+            DBSet.Any(expression);
+
+        public virtual IQueryable<T> OrderBy<TValue>(Expression<Func<T, TValue>> orderByExpression) =>
+            DBSet.OrderBy(orderByExpression).IncludeSurface();
+
+        public virtual async Task<IEnumerable<T>> GetAll(int? page = null) =>
+            page == null ? await DBSet.IncludeSurface().ToListAsync() : await DBSet.PaginateQuerable(page.Value, DEFAULT_PAGE_SIZE).IncludeSurface().ToListAsync();
+
+        public virtual async Task<PaginateDTO<T>> Paginate(int currentPage, Expression<Func<T, bool>>? expression)
+        {
+            var items = DBSet.ConditionalWhere(expression != null, expression!).Skip(currentPage * 10).IncludeSurface();
+            var itemsCount = await DBSet.ConditionalCount(expression!);
+
+            return new PaginateDTO<T>()
+            {
+                Items = items,
+                Page = currentPage,
+                Total = itemsCount,
+                Pages = (int)Math.Ceiling((double)itemsCount / 10),
+            };
+        }
+
+        // meant to be used by external API calls, that's why there is an extra validation layer
+        public async Task<IEnumerable<T>> SearchByProperty<TValue>(string propertyName, TValue value, int? page)
+        {
+            // TODO: nah, fix you laz..
+            if (value is string && string.IsNullOrEmpty(value as string))
+            {
+                return page == null ? await DBSet.IncludeSurface().ToListAsync() : await DBSet.IncludeSurface().PaginateQuerable(page.Value, DEFAULT_PAGE_SIZE).ToListAsync();
+            }
+
+            IEntity enetityInstance = (IEntity)Activator.CreateInstance(typeof(T))!;
+            List<string> properties = enetityInstance.SearchableProperties();
+
+            if (!ReflectionUtil.ContainsProperty(enetityInstance, propertyName))
+                throw new ArgumentException($"Property is not allowed to be searched or does not exists!");
+
+            if (!properties.Exists(property => properties.Exists(searchableProperty => searchableProperty.ToLower().Equals(propertyName.ToLower()))))
+                throw new ArgumentException($"Property is not allowed to be searched or does not exists!");
+
+
+            ParameterExpression parameter = Expression.Parameter(typeof(T), "property");
+            MemberExpression body = Expression.PropertyOrField(parameter, propertyName);
+            MethodInfo containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string), typeof(StringComparison) })!;
+            MethodCallExpression expression = Expression.Call(body, containsMethod, Expression.Constant(value, typeof(string)), Expression.Constant(StringComparison.OrdinalIgnoreCase));
+
+            var predicate = Expression.Lambda<Func<T, bool>>(expression, parameter);
+
+            IEnumerable<T> enetities = page == null ? await DBSet.IncludeSurface().Where(predicate).IncludeSurface().ToListAsync() : await DBSet.IncludeSurface().Where(predicate).IncludeSurface().PaginateQuerable(page.Value, DEFAULT_PAGE_SIZE).ToListAsync();
+
+            return enetities;
+        }
+
+        public async Task<T> FindByProperty<TValue>(Expression<Func<T, TValue>> selector, TValue value)
+        {
+            var predicate = Expression.Lambda<Func<T, bool>>(Expression.Equal(selector.Body, Expression.Constant(value, typeof(TValue))), selector.Parameters);
+
+            T? entity = await DBSet.IncludeSurface().FirstOrDefaultAsync(predicate);
+
+            if (entity == null)
+                throw new Exception($"Find by property was not found!");
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> FindAllByProperty<TValue>(Expression<Func<T, TValue>> selector, TValue value)
+        {
+            var predicate = Expression.Lambda<Func<T, bool>>(Expression.Equal(selector.Body, Expression.Constant(value, typeof(TValue))), selector.Parameters);
+
+            IEnumerable<T> entites = await DBSet.Where(predicate).IncludeSurface().ToListAsync();
+
+            if (!entites.Any())
+                throw new ArgumentException($"Find all by property was not found!");
+
+            return entites;
+        }
+
+        public virtual async Task<T> FindById(int id)
+        {
+            var entity = await DBSet.FindAsync(id);
+
+            if (entity == null)
+                throw new ArgumentException("Entity Not Found");
+
+            return entity;
+        }
+
+        public virtual async Task<T?> NullableFindById(int id) =>
+            await DBSet.FindAsync(id);
+
+
+        public virtual async Task<T> Delete(int id)
+        {
+            var targetEntitiy = await DBSet.FindAsync(id);
+
+            if (targetEntitiy != null)
+            {
+                DBSet.Remove(targetEntitiy);
+
+                return targetEntitiy;
+            }
+
+            throw new Exception(nameof(IEntity));
+        }
+
+        public async Task<T> Update(IEntity entityToUpdate)
+        {
+            T entity = await DBSet.FirstOrDefaultAsync(entity => ((IEntity)entity).Id == entityToUpdate.Id);
+
+            if (entity != null)
+            {
+                var dtoProperties = ReflectionUtil.GetInterfacedObjectProperties(entityToUpdate.GetType());
+
+                foreach (var property in dtoProperties)
+                {
+                    var dtoPropertyValue = ReflectionUtil.GetValueOf(entityToUpdate, property.Name);
+
+                    if (dtoPropertyValue != null)
+                    {
+                        if (ReflectionUtil.ContainsProperty(entity, property.Name))
+                            ReflectionUtil.SetValueOf(entity, property.Name, dtoPropertyValue);
+                    }
+                }
+
+                DBSet.Update(entity);
+
+                return entity;
+            }
+
+            throw new ArgumentException(nameof(T));
+        }
+
+        public async Task<T> Update(T updatedEntity)
+        {
+            var entityToUpdate = await DBSet.AsNoTracking().FirstOrDefaultAsync(entity => ((IEntity)updatedEntity).Id == ((IEntity)entity).Id);
+
+            if (entityToUpdate != null)
+            {
+                ReflectionUtil.MapEntity<T>((IEntity)updatedEntity, (IEntity)entityToUpdate);
+
+                DBSet.Update(entityToUpdate);
+
+                await UnitOfWork.CompletedAsync();
+
+                return entityToUpdate;
+            }
+
+            throw new ArgumentException("Entity Not Found");
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            await DBSet.AddAsync(entity);
+
+            await UnitOfWork.CompletedAsync();
+
+            return entity;
+        }
+
+    }
+}
---
+++ Core/Services/AccountService.cs
@@ -0,0 +1,57 @@
+﻿using Core.Services.Abstract;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using OwnerGPT.Core.Authentication;
+using OwnerGPT.Core.Extensions;
+using OwnerGPT.Core.Services.Compositions;
+using OwnerGPT.Models;
+using OwnerGPT.Models.Abstracts.DTO;
+using System.Security.Claims;
+
+namespace Core.Services
+{
+    public class AccountService : CompositionBaseService<Account>
+    {
+        // Interface it with generic authentiucation as provider...
+        private readonly ADAuthentication ADAuthentication;
+        private readonly IHttpContextAccessor HttpContextAccessor;
+
+        public AccountService(IHttpContextAccessor httpContextAccessor, ADAuthentication adAuthentication, ServiceBase<Account> RDBMSServiceBase, PGVServiceBase<VectorEmbedding> PGVServiceBase)
+            : base(RDBMSServiceBase, PGVServiceBase) =>
+            (ADAuthentication, HttpContextAccessor) = (adAuthentication, httpContextAccessor);
+
+        public async Task<Account> SignIn(CredentialsDTO credentials)
+        {
+            if (!ADAuthentication.IsAuthenticated(credentials))
+                throw new ArgumentException("Invalid authentication attempt!");
+
+            return await this.RDBMSServiceBase.FindByProperty(entity => entity.Email!, credentials.Identifier)
+                .Then(CookieAuthenticationSignIn).Then(account => account.Result);
+        }
+
+        public async Task SignOut() =>
+            await HttpContextAccessor.HttpContext.SignOutAsync();
+
+        private async Task<Account> CookieAuthenticationSignIn(Account account)
+        {
+            await HttpContextAccessor.HttpContext.SignInAsync(GenerateClaimsPrincipal(account));
+
+            return account;
+        }
+
+        private ClaimsPrincipal GenerateClaimsPrincipal(Account account) =>
+            new ClaimsPrincipal(GenerateClaimsIdentity(account));
+
+
+        private ClaimsIdentity GenerateClaimsIdentity(Account account) =>
+            new ClaimsIdentity(GenerateClaims(account), CookieAuthenticationDefaults.AuthenticationScheme);
+
+
+        private List<Claim> GenerateClaims(Account account) =>
+            new() {
+                new Claim(ClaimTypes.Email, account.Email!),
+            };
+
+    }
+}
