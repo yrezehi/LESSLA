40e2d596382eed424319263ab8aa96aa605484d5
unknown <yrezehi@gmail.com>
2023-10-18 08:46:30 -0700

    setup: EventLog

---
+++ Core/Models/EventLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class EventLog
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string MessageTemplate { get; set; }
+        public string Level { get; set; }
+        public datetime TimeStamp { get; set; }
+        public string Exception { get; set; }
+        public string Properties { get; set; }
+    }
+}
