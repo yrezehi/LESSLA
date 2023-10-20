21d313790fe2fd1b3461468bd3b3c216fecd1a24
unknown <yrezehi@gmail.com>
2023-10-20 06:28:23 -0700

    add: CacheExtensions

--- UI/Configuration/CacheExtensions.cs
+++ UI/Configuration/CacheExtensions.cs
@@ -1,11 +1,15 @@
-﻿using Core.Cache.Providers;
+﻿using Core.Cache.LRU;
+using Core.Cache.Providers;
 using Core.SSE;
 
 namespace UI.Configuration
 {
     public static class CacheExtensions
     {
-        public static void RegisterCache(this WebApplicationBuilder builder) =>
-            builder.Services.AddSingleton(typeof(CacheProvider), typeof(CacheProvider));       
+        public static void RegisterCache(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton(typeof(CacheProvider), typeof(CacheProvider));
+            builder.Services.AddSingleton(typeof(CacheLRU<,>), typeof(CacheLRU<,>));
+        }
     }
 }
