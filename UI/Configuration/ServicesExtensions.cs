9f84b69f70892f753cd656c7a5f9e416b9669cdb
unknown <yrezehi@gmail.com>
2023-10-20 06:41:50 -0700

    add: ServicesExtensions

---
+++ UI/Configuration/ServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Core.Services;
+using Core.SSE;
+
+namespace UI.Configuration
+{
+    public static class ServicesExtensions
+    {
+        public static void RegisterServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient(typeof(ApplicationsService), typeof(ApplicationsService));
+            builder.Services.AddTransient(typeof(EventLogsService), typeof(EventLogsService));
+        }
+    }
+}
