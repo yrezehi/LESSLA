8be7e479e5bf6cc07d14a1cfccc6eb6de9664c78
unknown <yrezehi@gmail.com>
2023-10-20 05:42:53 -0700

    add: guid id client sse

--- Core/SSE/SSEClient.cs
+++ Core/SSE/SSEClient.cs
@@ -3,12 +3,12 @@ namespace Core.SSE
 {
     public class SSEClient
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public HttpResponse Client { get; set; }
         public DateTime JoinTime { get; set; }
 
         private SSEClient(HttpResponse response) =>
-            (Id, Client, JoinTime) = ( ,response, DateTime.Now);
+            (Id, Client, JoinTime) = (Guid.NewGuid().ToString(), response, DateTime.Now);
 
         public static SSEClient New(HttpResponse response) =>
             new SSEClient(response);
