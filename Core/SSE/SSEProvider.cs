df22ed0b62924cbefc3a5eed56669f4df3ec2ca9
unknown <yrezehi@gmail.com>
2023-10-20 05:29:58 -0700

    add: sse brodcast

---
+++ Core/SSE/SSEProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Concurrent;
+
+namespace Core.SSE
+{
+    public class SSEProvider
+    {
+        public readonly ConcurrentBag<HttpResponse> Clients;
+
+        public SSEProvider() =>
+            Clients = new ConcurrentBag<HttpResponse>();
+        
+        public void Brodcast(string @event)
+        {
+            Parallel.ForEach(Clients, ParallelOptions, client =>
+            {
+                client.SendSEEEvent(@event).Wait();
+            });
+        }
+
+        private ParallelOptions ParallelOptions => new ParallelOptions() { MaxDegreeOfParallelism = 5 };
+    }
+}
