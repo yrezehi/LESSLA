5dc0faef3dfc94d1bef9557b7c539cfd793c5e24
unknown <yrezehi@gmail.com>
2023-10-21 05:55:17 -0700

    setup: briefdto

---
+++ Core/Models/Abstracts/DTO/BriefDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Abstracts.DTO
+{
+    public class BriefDTO
+    {
+        public int ErrorCount { get; set; } = 0;
+        public int WarningCount { get; set; } = 0;
+
+        public readonly int IssueCount { get; set; } = 0;
+
+        public BriefDTO(int errorCount = 0, int warningCount = 0) =>
+            (ErrorCount, WarningCount, IssueCount) = (errorCount, warningCount, errorCount + warningCount);
+    }
+}
