fbcd12ae13cfa9f9eeafc35e509ca0ab0a180b0f
unknown <yrezehi@gmail.com>
2023-10-21 10:23:07 -0700

    setup: HandleChange

--- Core/Events/SQL/SQLDepedencyAdapter.cs
+++ Core/Events/SQL/SQLDepedencyAdapter.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using static Core.Events.SQL.SQLDepedency;
 
 namespace Core.Events.SQL
 {
-    public class SQLDepedencyAdapter
+    public class SQLDepedencyAdapter<T> : IDisposable where T : class
     {
         public readonly SQLDepedency Listener;
 
@@ -17,9 +20,34 @@ namespace Core.Events.SQL
         {
             Listener = new SQLDepedency(connectionString, DATABASE_NAME, DATABASE_TABLE_NAME);
 
-            Listener.TableChanged += (_, _) => HandleChange();
+            Listener.TableChanged += HandleChange;
+
+            Listener.Start();
         }
 
-        private void HandleChange() { }
+        private void HandleChange(object? _, TableChangedEventArgs @event) { 
+            if(@event.NotificationType == NotificationTypes.Insert && @event.Data != null)
+            {
+                var dataElement = @event.Data.Element("inserted");
+
+                if(dataElement != null)
+                {
+                    var rows = dataElement.Elements("row");
+
+                    foreach(var row in rows ) {
+                        if (row != null)
+                        {
+                            var entity = CastRow(row);
+                        }
+                    }
+                }
+            }
+        }
+
+        private T? CastRow(XElement element) =>
+            (T) new XmlSerializer(element.GetType()).Deserialize(element.CreateReader())!;
+
+        public void Dispose() =>
+            Listener.Stop();
     }
 }
