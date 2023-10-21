1fbf6dd35ab2c922e5e20f00dcc698662828a7a8
unknown <yrezehi@gmail.com>
2023-10-21 07:55:21 -0700

    add: sql dep adapter

---
+++ Core/Events/SQL/SQLDepedencyAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Events.SQL
+{
+    public class SQLDepedencyAdapter
+    {
+        public readonly SQLDepedency Listener;
+
+        private readonly static string DATABASE_NAME = "lessla";
+        private readonly static string DATABASE_TABLE_NAME = "logs";
+
+        public SQLDepedencyAdapter(string connectionString)
+        {
+            Listener = new SQLDepedency(connectionString, DATABASE_NAME, DATABASE_TABLE_NAME);
+
+            Listener.TableChanged += (_, _) => HandleChange();
+        }
+
+        private void HandleChange() { }
+    }
+}
