6c77aa2abb18ef2e268f8acb4269cc691b9bdcc2
unknown <yrezehi@gmail.com>
2023-10-23 07:14:57 -0700

    finish: animation

--- UI/wwwroot/js/animation.js
+++ UI/wwwroot/js/animation.js
@@ -4,9 +4,9 @@
         var element = document.querySelector(selector);
 
         binding.clickToTrigger(document.querySelector(onClickSelector), function () {
-            if (element.style.maxHeight === "0") {
-                element.style.maxHeight = element.scrollHeight;
-                element.style.overflow = "visible";
+            if (element.style.maxHeight === "0px") {
+                element.style.transition = "max-height 0.6s linear";
+                element.style.maxHeight = `${element.scrollHeight}px`;
             } else {
                 element.style.maxHeight = "0";
                 element.style.overflow = "hidden";
