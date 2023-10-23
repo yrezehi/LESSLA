dc51bcce3268381a61fb5202c49a3549c9c596df
unknown <yrezehi@gmail.com>
2023-10-23 06:05:15 -0700

    setup: hight

---
+++ UI/wwwroot/js/animation.js
@@ -0,0 +1,29 @@
+﻿var animation = function () {
+
+    function toggle(selector, show = true) {
+        var element = document.querySelector(selector);
+
+        if (!element)
+            notification.error(`Could not find element ${selector} to animate!`);
+
+        binding.clickToTrigger(element, function () {
+            if (show) {
+                element.style.maxHeight = element.scrollHeight;
+                element.s
+            } else {
+
+            }
+        });
+    }
+
+    function show(selector) {
+
+    }
+
+    return function () {
+        return {
+            hideViaHeight: hide,
+            showViaHeight: show
+        };
+    }();
+}();
