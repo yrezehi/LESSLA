940bce9a3c3883dd13ef683da1f7b5ab6ebec590
unknown <yrezehi@gmail.com>
2023-10-23 09:11:20 -0700

    finilize: animation

--- UI/wwwroot/js/animation.js
+++ UI/wwwroot/js/animation.js
@@ -2,13 +2,13 @@
 
     function toggleHeight(clickableSelector, triggerSelector) {
         binding.clickToTrigger(document.querySelector(clickableSelector), function (_) {
-            var triggerSelector = document.querySelector(triggerSelector);
-            if (triggerSelector.style.maxHeight === "0px") {
-                triggerSelector.style.transition = "max-height 0.25s linear";
-                triggerSelector.style.maxHeight = `${triggerSelector.scrollHeight}px`;
+            var triggerElement = document.querySelector(triggerSelector);
+            if (triggerElement.style.maxHeight === "0px") {
+                triggerElement.style.transition = "max-height 0.25s linear";
+                triggerElement.style.maxHeight = `${triggerElement.scrollHeight}px`;
             } else {
-                triggerSelector.style.maxHeight = "0";
-                triggerSelector.style.overflow = "hidden";
+                triggerElement.style.maxHeight = "0";
+                triggerElement.style.overflow = "hidden";
             }
         });
     }
