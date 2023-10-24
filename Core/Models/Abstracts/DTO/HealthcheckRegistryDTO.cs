cf7d8fa620ffb47083a99b2fa03d58c1c00961fb
unknown <yrezehi@gmail.com>
2023-10-23 18:19:08 -0700

    setup: HealthcheckRegistryDTO

---
+++ Core/Models/Abstracts/DTO/HealthcheckRegistryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Abstracts.DTO
+{
+    public class HealthcheckRegistryDTO
+    {
+        public string ApplicationName { get; set; }
+        public string Endpoint { get; set; }
+    }
+}
