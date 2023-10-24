d305542c2a6c2edda79662b417c2d4a0cb7114ae
unknown <yrezehi@gmail.com>
2023-10-24 10:51:37 -0700

    add: CheckHealthAsync

--- Sample/Lessla/HealthCheck.cs
+++ Sample/Lessla/HealthCheck.cs
@@ -4,9 +4,39 @@ namespace Sample.Lessla
 {
     public class HealthCheck : IHealthCheck
     {
+        private readonly HttpClient HttpClient;
+
         public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            IList<string> services = new List<string>();
+
+            IEnumerable<Task<bool>> concurrentResponses = services.Select(IsHeadRequestReachable);
+
+            if (await AnyUnreachableRequest(concurrentResponses))
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+
+        private async Task<bool> AnyUnreachableRequest(IEnumerable<Task<bool>> concurrentResponses)
+        {
+            var responses = await Task.WhenAll(concurrentResponses);
+
+            return responses.ToList().Exists(response => !response);
+        }
+
+        private async Task<bool> IsHeadRequestReachable(string url)
+        {
+            HttpResponseMessage response = await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
