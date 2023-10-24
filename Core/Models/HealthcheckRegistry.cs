1f82975623ac0f358c7af11e08daada858ada209
unknown <yrezehi@gmail.com>
2023-10-23 18:31:30 -0700

    setup: HTTPUtils

---
+++ Core/Models/HealthcheckRegistry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class HealthcheckRegistry
+    {
+        public string ApplicationName { get; set; }
+        public int MinutesInterval { get; set; } = 60;
+        public string Endpoint { get; set; }
+    }
+}
---
+++ Core/Utils/HTTPUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utils
+{
+    public static class HTTPUtils
+    {
+        private readonly static int REQUEST_TIMEOUT = 10;
+
+        public static bool IsPingable(string endpoint) =>
+            new Ping().Send(endpoint, REQUEST_TIMEOUT).Status == IPStatus.Success;
+    }
+}
