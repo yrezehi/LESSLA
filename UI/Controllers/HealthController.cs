f05a097dd50d8854a163dfe20ec084aec6ccfdde
unknown <yrezehi@gmail.com>
2023-10-25 05:14:42 -0700

    setup: HealthController

---
+++ UI/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models.Serilog;
+using Core.Services;
+using Core.SSE;
+using Microsoft.AspNetCore.Mvc;
+using UI.Controllers.Abstracts;
+
+namespace UI.Controllers
+{
+    [Route("[controller]")]
+    public class HealthController : Controller
+    {
+        private readonly HealthService Service;
+
+        public HealthController(HealthService service) =>
+            Service = service;
+    }
+}
