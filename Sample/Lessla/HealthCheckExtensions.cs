02bf081da3b23ef74785c8bfc14781656a07f716
unknown <yrezehi@gmail.com>
2023-10-27 02:47:00 -0700

    setup: SetPendingRequest

--- Sample/Lessla/HealthCheckExtensions.cs
+++ Sample/Lessla/HealthCheckExtensions.cs
@@ -7,7 +7,7 @@
         private static string CONFIGURATION_HEALTH_CHECK_CONNECTION_STRINGS_KEY = "ConnectionStrings";
         private static string CONFIGURATION_HEALTH_CHECK_EXTERNAL_ENDPOINTS_KEY = "ExternalEndpoints";
 
-        public static void RegisterDependencies(this WebApplicationBuilder builder)
+        public static void RegisterLESSLA(this WebApplicationBuilder builder)
         {
             if (builder.Configuration.GetSection(CONFIGURATION_ROOT_HEALTH_CHECK_PATH).Exists())
             {
