fd3d34ba602b93580bed28a5e7d7d740610bbaf7
unknown <yrezehi@gmail.com>
2023-10-27 05:18:48 -0700

    setup: HealthCheckLog

---
+++ Core/Models/HealthCheckLog.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models
+{
+    public class HealthCheckLog
+    {
+        public int Id { get; set; }
+        public int ApplicationId { get; set; }
+        public DateTime ExecutionTime { get; set; }
+        public int ExecutionDuration { get; set; }
+        public bool IsHealthy { get; set; }
+    }
+}
