c0ab2cf292c0bc28b6045dfd360d4626aed9f504
ykrezaihi <ykrezaihi@sfda.gov.sa>
2023-11-01 13:01:23 +0300

    add: JSONExtenstions

---
+++ Core/Utils/Extenstions/JSONExtenstions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.Utils.Extenstions
+{
+    public static class JSONExtenstions
+    {
+        public static bool TryParseJSON<T>(this string serializedContent, out T? dserializedContent)
+        {
+            bool isSuccess = true;
+
+            dserializedContent = JsonConvert.DeserializeObject<T>(serializedContent, new JsonSerializerSettings()
+            {
+                Error = (_, arguments) => { isSuccess = false; arguments.ErrorContext.Handled = true; },
+                MissingMemberHandling = MissingMemberHandling.Error
+            });
+
+            return isSuccess;
+        }
+    }
+}
