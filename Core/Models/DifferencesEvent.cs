3b0acc5a469243b546f65629d10ded44402000a0
unknown <yrezehi@gmail.com>
2023-11-02 11:07:34 -0700

    rm: bad idea

---
+++ Core/Models/DifferencesEvent.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models.Serilog;
+
+namespace Core.Models
+{
+    public class DifferencesEvent
+    {
+        public readonly string ApplicationName;
+        public List<EventLog> ExceptionEvents { get; set; } = new List<EventLog>();
+        public int DifferencesCount { get; set; } = 1;
+
+        public DifferencesEvent(string applicationName, List<EventLog> exceptionEvents, int differencesCount) =>
+          (ApplicationName, ExceptionEvents, DifferencesCount) = (applicationName, exceptionEvents, differencesCount);
+    }
+}
