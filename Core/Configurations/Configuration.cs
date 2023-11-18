07ecb119f09ce9f0bd8d27bd0ad00814ad2b4fdb
unknown <yrezehi@gmail.com>
2023-11-18 04:28:00 -0800

    setup: configuration file

---
+++ Core/Configurations/Configuration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Core.Configuration
+{
+    public class Configuration
+    {
+        private static IConfiguration? ConfigurationInstance;
+
+        public static void Register(IConfiguration configuration) =>
+            ConfigurationInstance = configuration;
+
+        public static IConfigurationSection GetSection(string section) =>
+            ConfigurationInstance!.GetSection(section);
+
+        public static T GetValue<T>(string path)
+        {
+            T? value = ConfigurationInstance!.GetValue<T>(path);
+
+            if (value == null)
+            {
+                throw new ArgumentException($"No such configuration property path {path}!");
+            }
+
+            return value!;
+        }
+
+        public static IList<T> GetList<T>(string path)
+        {
+            List<T>? value = GetSection(path).Get<List<T>>();
+
+            if (value == null || value.Count == 0)
+            {
+                throw new ArgumentException($"No such configuration property path {path}!");
+            }
+
+            return value!;
+        }
+    }
+}
