451f88190bd8822c845bf6f2df1f403de6548748
unknown <yrezehi@gmail.com>
2024-01-26 22:13:03 -0800

    set: geh

---
+++ Core/Exceptions/GlobalErrorHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace Core.Exceptions
+{
+    public class GlobalErrorHandler
+    {
+        private readonly RequestDelegate RequestDelegate;
+
+        public GlobalErrorHandler(RequestDelegate requestDelegate) =>
+            RequestDelegate = requestDelegate;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try { await RequestDelegate(context); }
+            catch (Exception exception) { await HandleException(context, exception); }
+        }
+
+        private static async Task HandleException(HttpContext context, Exception exception)
+        {
+            dynamic loggingErrorObject = new { };
+
+            string traceId = context.TraceIdentifier.ToString();
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+
+            string? exceptionMessage = "Internal Server Error";
+
+            loggingErrorObject = new
+            {
+                StatusCode = statusCode,
+                Cause = "Unhandled exception",
+                TraceId = traceId
+            };
+
+            string serializedErrorObject = JsonSerializer.Serialize(loggingErrorObject);
+            Log.Error(exception, serializedErrorObject);
+
+            context.Response.StatusCode = statusCode;
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new
+            {
+                StatusCode = statusCode,
+                Message = exceptionMessage,
+                TraceId = traceId
+            }));
+
+        }
+    }
+}
