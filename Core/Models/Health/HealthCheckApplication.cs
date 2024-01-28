a2ade1a642c034b2abfca309de3d535493e3e7f7
unknown <yrezehi@gmail.com>
2024-01-28 06:06:40 -0800

    set: application hc

---
+++ Core/Models/Health/HealthCheckApplication.cs
@@ -0,0 +1,34 @@
+﻿using Core.Models.Abstracts.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Models.Health
+{
+    [Table("health_application")]
+    public class HealthCheckApplication : IEntity
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("application_name")]
+        public string ApplicationName { get; set; }
+        [Column("minutes_interval")]
+        public int MinutesInterval { get; set; } = 60;
+        [Column("url")]
+        public string URL { get; set; }
+
+        public HealthCheckApplication() { }
+
+        public HealthCheckApplication(string applicationName, string url) =>
+            (ApplicationName, URL) = (applicationName, url);
+
+        public static HealthCheckApplication Create(string applicationName, string url) =>
+            new HealthCheckApplication(applicationName, url);
+
+        public HealthCheckApplication WithId(int id)
+        {
+            Id = id;
+            return this;
+        }
+    }
+}
