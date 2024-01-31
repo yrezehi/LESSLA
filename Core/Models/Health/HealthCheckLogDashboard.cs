5a1065dfeb1273e5fa1307ac37c6e432d2efa318
unknown <yrezehi@gmail.com>
2024-01-31 09:48:47 -0800

    set: dashboard

---
+++ Core/Models/Health/HealthCheckLogDashboard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Health
+{
+    public class HealthCheckLogDashboard
+    {
+        public int RegistredApplications { get; set; } = 0;
+        public int FailedChecksToday { get; set; } = 0;
+        public int Downtime { get; set; } = 0;
+
+        public static HealthCheckLogDashboard Create() =>
+            new();
+
+        public HealthCheckLogDashboard WithRegistredApplications(int count)
+        {
+            RegistredApplications = count;
+            return this;
+        }
+
+        public HealthCheckLogDashboard WithFailedChecksToday(int count)
+        {
+            FailedChecksToday = count;
+            return this;
+        }
+
+        public HealthCheckLogDashboard WithDowntime(int count)
+        {
+            Downtime = count;
+            return this;
+        }
+    }
+}
---
+++ Core/Services/Health/HealthLogService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models.Health;
+using Core.Repositories.Abstracts.Interfaces;
+using Core.Services.Abstract;
+
+namespace Core.Services.Health
+{
+    public class HealthLogService : ServiceBase<HealthCheckLog>
+    {
+        private readonly HealthService HealthService;
+
+        public HealthLogService(IUnitOfWork unitOfWork, HealthService healthService) : base(unitOfWork) =>
+            (HealthService) = (healthService);
+
+        public async Task<HealthCheckLogDashboard> Dashboard() =>
+            HealthCheckLogDashboard.Create()
+                .WithRegistredApplications(await HealthService.Count());
+    }
+}
