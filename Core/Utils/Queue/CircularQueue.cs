dc12e96eea1a801d2aed0823cfe48eccd4141ea3
unknown <yrezehi@gmail.com>
2024-02-02 21:25:17 -0800

    set: CircularQueue

---
+++ Core/Utils/Queue/CircularQueue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+namespace Core.Utils.Queue
+{
+    public class CircularQueue<T> : ConcurrentQueue<T> where T : class 
+    {
+        public readonly int Limit;
+
+        public CircularQueue(int limit) =>
+            Limit = limit;
+        
+
+        public new void Enqueue(T element)
+        {
+            base.Enqueue(element);
+            if (Count > Limit)
+            {
+                TryDequeue(out _);
+            }
+        }
+
+    }
+}
